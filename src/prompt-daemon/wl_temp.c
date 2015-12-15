b48f6f5514fe02c7240f29bcdaaf08ff1169b38f
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2015-12-15 18:09:16 +0100

    wl-template: make it more stable

--- src/prompt-daemon/wl_temp.c
+++ src/prompt-daemon/wl_temp.c
@@ -1,11 +1,18 @@
 #include "main.h"
 
 #include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
 #include <libgen.h>
 
 static void
 _fire_up(void *data) {
-    execl(data, basename(data), NULL);
+    uid_t uid;
+    struct passwd *pwd;
+
+    uid = getuid();
+    pwd = getpwuid(uid);
+    execl(pwd->pw_shell, basename(pwd->pw_shell), "-c", data);
 }
 
 void
