996cf71cd4df7d080cd47a9848af518ab70d0a49
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2016-07-23 14:03:21 +0200

    update the logind part to be more stable

---
+++ src/daemon/sessionmgt_logind.c
@@ -0,0 +1,78 @@
+#include "main.h"
+#include <unistd.h>
+#include <systemd/sd-login.h>
+#include <errno.h>
+
+char*
+current_session_get(void) {
+    pid_t pid = getpid();
+    char *result;
+
+    if (sd_pid_get_session(pid, &result) < 0)
+      return NULL;
+
+    return result;
+}
+
+void
+session_enumerate(char ***handles, unsigned int *len) {
+    if ((*len = sd_seat_get_sessions("seat0", handles, NULL, NULL)) <= 0) {
+        printf("Failed to get sessions.\n");
+        *handles = NULL;
+        *len = 0;
+    }
+}
+
+void
+session_activate(char *handle) {
+    char buf[PATH_MAX];
+
+    if (!handle) return;
+
+    snprintf(buf, sizeof(buf), "loginctl activate %s", handle);
+
+    system(buf);
+}
+
+#define GET_FIELD(func, field, errval) \
+    if (field) { \
+        ret = func(handle, field); \
+        switch(ret){ \
+            case -ENXIO: \
+                printf("Error, The specified session does not exist.\n"); \
+                *field = errval; \
+            break; \
+            case -ENODATA: \
+                printf("Error, The given field is not specified for the described session.\n"); \
+                *field = errval; \
+            break; \
+            case -EINVAL: \
+                printf("Error, An input parameter was invalid (out of range, or NULL, where that is not accepted).\n"); \
+                *field = errval; \
+            break; \
+            case -ENOMEM: \
+                printf("Error, Memory allocation failed.\n"); \
+                *field = errval; \
+            break; \
+        }\
+    }
+
+int
+session_details(char *handle, uid_t *uid, char **name, char **icon, char **tty) {
+    int ret;
+
+    GET_FIELD(sd_session_get_tty, tty, NULL)
+    GET_FIELD(sd_session_get_uid, uid, 0)
+    GET_FIELD(sd_session_get_class, name, NULL)
+
+    if (icon)
+      *icon = NULL;
+    return 1;
+}
+
+void
+wait_session_active(char *handle) {
+    while (!sd_session_is_active(handle)) {
+        sleep(1);
+    }
+}
